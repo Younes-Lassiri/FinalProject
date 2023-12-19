6e43cb7a21daffee5bd9d3521fb76a4648098881
Younes <lassiriyounesse04@gmail.com>
2023-12-19 09:40:51 +0100

    yyyyyyyy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/royal-chic/",
+  base: '/royal-chic/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
+});
